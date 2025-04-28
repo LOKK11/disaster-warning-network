6e03bc3ec0c3526f35d270e71b996e65ecd3dbe5
Aleksi <alholapp22@student.oulu.fi>
2025-04-28 15:29:35 +0300

    Modify task 3

--- src/diffusion_dataset.py
+++ src/diffusion_dataset.py
@@ -21,8 +21,36 @@ RELIABILITY_MIN, RELIABILITY_MAX = 0, 1
 message_records = []
 alert_time = datetime.now()
 
-# Generate at least 250 message records
-while len(message_records) < 250:
+# From message records, get the sent timestamp of a node
+def get_timestamp_sent(node_id):
+    for message in message_records:
+        if message["source_node_id"] == node_id:
+            return datetime.fromisoformat(message["timestamp_sent"])
+    return alert_time
+
+# Randomly generate connections between all nodes
+for i in range(len(PROPAGATION_PATH) - 1):
+    source_nodes = nodes_df[nodes_df["node_type"] == PROPAGATION_PATH[i]]
+    target_nodes = nodes_df[nodes_df["node_type"] == PROPAGATION_PATH[i + 1]]
+    time_sent = alert_time
+    for _, target in target_nodes.iterrows():
+        random_node = source_nodes.sample(1).iloc[0]
+        processing_delay = random.randint(DELAY_MIN, DELAY_MAX / 3)
+        time_sent = get_timestamp_sent(random_node["node_id"]) + timedelta(seconds=processing_delay)
+        transmission_delay = random.randint(DELAY_MIN, DELAY_MAX)
+        time_received = time_sent + timedelta(seconds=transmission_delay)
+        message_records.append({
+            "source_node_id": random_node["node_id"],
+            "destination_node_id": target["node_id"],
+            "message_type": random.choice(MESSAGE_TYPES),
+            "timestamp_sent": time_sent.isoformat(),
+            "timestamp_received": time_received.isoformat(),
+            "delay_in_seconds": transmission_delay,
+            "reliability_score": round(random.uniform(RELIABILITY_MIN, RELIABILITY_MAX), 2),
+        })
+
+# Randomly generate at least 300 message records
+while len(message_records) < 300:
     path_nodes = []
     for node_type in PROPAGATION_PATH:
         type_nodes = nodes_df[nodes_df["node_type"] == node_type]
@@ -30,19 +58,19 @@ while len(message_records) < 250:
         path_nodes.append(random_node)
     time_sent = alert_time
     for i in range(len(path_nodes) - 1):
-        delay = random.randint(DELAY_MIN, DELAY_MAX)
-        time_received = time_sent + timedelta(seconds=delay)
+        transmission_delay = random.randint(DELAY_MIN, DELAY_MAX)
+        time_received = time_sent + timedelta(seconds=transmission_delay)
         message_records.append({
             "source_node_id": path_nodes[i]["node_id"],
             "destination_node_id": path_nodes[i + 1]["node_id"],
             "message_type": random.choice(MESSAGE_TYPES),
             "timestamp_sent": time_sent.isoformat(),
             "timestamp_received": time_received.isoformat(),
-            "delay_in_seconds": delay,
+            "delay_in_seconds": transmission_delay,
             "reliability_score": round(random.uniform(RELIABILITY_MIN, RELIABILITY_MAX), 2),
         })
         # Delay to not send the received message immediately
-        processing_delay = random.randint(DELAY_MIN, 60)
+        processing_delay = random.randint(DELAY_MIN, DELAY_MAX / 3)
         time_sent = time_received + timedelta(seconds=processing_delay)
 
 messages_df = pd.DataFrame(message_records)
